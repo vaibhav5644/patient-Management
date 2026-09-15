cba38f3fbf8e20619ef53e5417f70ae2c01faed1
vaibhav5644 <vaibhavgaikwad250@gmail.com>
2026-09-16 00:03:57 +0530

    HashMap Added

---
+++ patient-service/src/main/java/com/pm/patientservice/service/Demo_Feature.java
@@ -0,0 +1,18 @@
+package com.pm.patientservice.service;
+
+import java.util.HashMap;
+
+public class Demo_Feature {
+
+	public static void main(String[] args) {
+
+		HashMap<String, String> map = new HashMap();
+		map.put("Ram", "12");
+		map.put("shyam", "122");
+		map.put("Monya", "123");
+		
+		map.forEach((x,y)->System.out.println(x+" "+y));
+	}
+	
+
+}
